67d7524776f2ef89e0a96d17b5af79967a2a7a08
sawantxsumit <sawantsumit297@gmail.com>
2026-09-13 23:40:49 +0530

    merge sorted array

--- dsa_with_python/twosum.py
+++ dsa_with_python/twosum.py
@@ -1,3 +1,26 @@
+'''
+You are given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+'''
+
+
+
 class solution():
     def twoSum(self , nums,target):
         # for i in range(len(nums)):
@@ -5,15 +28,12 @@ class solution():
         #         if nums[i]+nums[j]==target:
         #             return [i,j]
         hash_table = {}
-
-        for i in range(len(nums)):
-            hash_table[nums[i]]= i
-
                    
         for i in range(len(nums)): 
             complement= target-nums[i]
-            if complement in nums and hash_table[complement] !=i:
+            if complement in nums:
                 return [i,hash_table[complement]]
+            hash_table[nums[i]]= i
             
             
                 
