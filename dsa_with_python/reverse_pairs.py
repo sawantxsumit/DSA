b930a07ed046dc4450c31cf4b5f81ecaa9266310
sawantxsumit <sawantsumit297@gmail.com>
2026-09-15 14:26:02 +0530

    reverse pairs

--- dsa_with_python/reverse_pairs.py
+++ dsa_with_python/reverse_pairs.py
@@ -23,8 +23,56 @@ Explanation: The reverse pairs are:
 #     return count
 
 #method 2
-# def reverse_pairs(nums):
-#     nums2=nums*2
+def reverse_pairs(nums):
+    def merge_sort(left, right):
+            if left >= right:
+                return 0
+
+            mid = (left + right) // 2
+
+            count = 0
+
+            # left half
+            count += merge_sort(left, mid)
+
+            # right half
+            count += merge_sort(mid + 1, right)
+
+            j = mid + 1
+
+            for i in range(left, mid + 1):
+                while j <= right and nums[i] > 2 * nums[j]:
+                    j += 1
+
+                count += j - (mid + 1)
+
+            #merge
+            temp = []
+            i = left
+            j = mid + 1
+
+            while i <= mid and j <= right:
+                if nums[i] <= nums[j]:
+                    temp.append(nums[i])
+                    i += 1
+                else:
+                    temp.append(nums[j])
+                    j += 1
+
+            while i <= mid:
+                temp.append(nums[i])
+                i += 1
+
+            while j <= right:
+                temp.append(nums[j])
+                j += 1
+
+            for k in range(len(temp)):
+                nums[left + k] = temp[k]
+
+            return count
+
+    return merge_sort(0, len(nums) - 1)
     
 
 
@@ -34,4 +82,4 @@ for n in nums:
     num2.append(n*2)
     
 print(num2)
-# print(reverse_pairs(nums))
+print(reverse_pairs(nums))
